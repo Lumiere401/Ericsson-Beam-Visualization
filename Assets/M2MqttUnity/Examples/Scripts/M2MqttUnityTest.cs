acf7ed3c227e9dc0335819d7b40f4ca1c4657332
Venkata Satya Anurag <91436036+moonglory97@users.noreply.github.com>
2023-10-20 19:07:22 +0200

    replaced lobby model and added mqtt support

--- Assets/M2MqttUnity/Examples/Scripts/M2MqttUnityTest.cs
+++ Assets/M2MqttUnity/Examples/Scripts/M2MqttUnityTest.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7cc1fc4c2caf7e51b858d4b1cb132fce404a629dad34cb843ad6fd64f68c1b80
-size 8075
+oid sha256:22bd94551015e7c69e573309cd6576c99f5b2d46e5f62d4ee0180244514a66de
+size 8061
